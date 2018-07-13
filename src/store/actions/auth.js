1eabbc339352efe0ac6174098c236bcfc8e51adb
Devon Vaccarino <devonev92@gmail.com>
2018-07-13 13:29:56 -0700

    removed api key

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -47,9 +47,9 @@ export const auth = (email, password, isSignup) => {
 			password: password,
 			returnSecureToken: true
 		};
-		let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCkLZ_djJK2yK8mO5mNSDOEUEllMvJ1Akg';
+		let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=';
 		if(isSignup) {
-			url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCkLZ_djJK2yK8mO5mNSDOEUEllMvJ1Akg'
+			url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key='
 		}
 		axios.post(url, authData)
 			.then(response => {
